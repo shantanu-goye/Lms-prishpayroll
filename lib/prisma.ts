b39ce9ae91d41d78cc0c722e2d15326170110a15
Shantanu Gote <shantanugote82@gmail.com>
2026-03-05 23:23:51 +0530

    Fix Prisma adapter version mismatch for Netlify build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const prismaClientSingleton = () => {
@@ -9,7 +9,7 @@ const prismaClientSingleton = () => {
       url: process.env.TURSO_DATABASE_URL,
       authToken: process.env.TURSO_AUTH_TOKEN,
     })
-    const adapter = new PrismaLibSql(libsql)
+    const adapter = new PrismaLibSQL(libsql)
     // @ts-ignore - adapter is valid with driverAdapters preview feature
     return new PrismaClient({ adapter })
   }
